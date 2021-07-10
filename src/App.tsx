ac7d4c338b59aa468fd427e20effd733b932bc1e
Andre Eickler <797483+eickler@users.noreply.github.com>
2021-07-10 15:23:08 +0200

    Forgot to clear selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import { verbs, VerbDefinition } from './Verbs';
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import { verbs, VerbDefinition } from "./Verbs";
 import { Question } from "./Question";
 import { Choice, Selection } from "./Choice";
 import { Answer } from "./Answer";
@@ -14,48 +14,58 @@ interface State {
   selection: Selection;
 }
 
-const pickVerb = (verbs: VerbDefinition[]) : VerbDefinition => {
+const pickVerb = (verbs: VerbDefinition[]): VerbDefinition => {
   const rnd = Math.floor(Math.random() * verbs.length);
   return verbs[rnd];
-}
+};
 
-const initialize = (level: number) : State => {
-  const verbsToPractice = verbs.filter( verb => verb.level <= level);
+const initialize = (level: number): State => {
+  const verbsToPractice = verbs.filter((verb) => verb.level <= level);
   return {
     verbsToPractice: verbsToPractice,
     verb: pickVerb(verbsToPractice),
     selection: Selection.NotSelected,
-  } 
-}
+  };
+};
 
 const App = () => {
   const [state, setState] = useState(initialize(18));
 
   const onSelect = (selection: Selection) => {
     setState({ ...state, selection: selection });
-  }
+  };
 
   const onContinue = () => {
-    setState({ ...state, verb: pickVerb(state.verbsToPractice) });
-  }
+    setState({
+      ...state,
+      verb: pickVerb(state.verbsToPractice),
+      selection: Selection.NotSelected,
+    });
+  };
 
   const onConfigUpdate = (config: Configuration) => {
     setState(initialize(config.level));
-  }
+  };
 
   return (
     <>
-      <Setup defaultLevel={18} onConfigUpdate={onConfigUpdate}/>
-      <Question verb={state.verb}/>
-      <Choice currentSelection={state.selection} onSelect={onSelect}/>
-      { state.selection !== Selection.NotSelected && 
-        <Answer verb={state.verb}/>
-      }
+      <Setup defaultLevel={18} onConfigUpdate={onConfigUpdate} />
+      <Question verb={state.verb} />
+      <Choice currentSelection={state.selection} onSelect={onSelect} />
+      {state.selection !== Selection.NotSelected && (
+        <Answer verb={state.verb} />
+      )}
       <Container>
-        <Button variant="contained" onClick={onContinue} startIcon={<NavigateNextIcon />}>Continue</Button>
+        <Button
+          variant="contained"
+          onClick={onContinue}
+          startIcon={<NavigateNextIcon />}
+        >
+          Continue
+        </Button>
       </Container>
     </>
-  )
-}
+  );
+};
 
 export default App;
