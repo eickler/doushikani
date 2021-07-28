a4a78d31431763956b6ff45a70ed7767f4c291ef
Andre Eickler <797483+eickler@users.noreply.github.com>
2021-07-28 19:24:48 +0200

    Elementary particle search for the relevant particles

---
+++ nlp/particler.py
@@ -0,0 +1,23 @@
+import json
+import spacy
+
+
+def search_particles(verbs):
+    nlp = spacy.load("ja_core_news_sm")
+    for verb in verbs:
+        for example in verb['examples']:
+            doc = nlp(example['ja'])
+            example['indexes'] = []
+            for token in doc:
+                if token.pos_ != 'ADP':
+                    continue
+                if token.text not in ['は','が','を','に']:
+                    continue
+                if token.dep_ not in ['case', 'fixed', 'advcl']:
+                    continue
+                example['indexes'].append(token.idx)
+
+
+verbs = json.load(open('../src/verbs.json', 'r'))
+search_particles(verbs)
+json.dump(verbs, open('../src/verbs.json', 'w'))
