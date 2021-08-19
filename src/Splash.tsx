56d901fdcf79f894fc85b67f3fa8ca14e030b562
Andre Eickler <797483+eickler@users.noreply.github.com>
2021-08-19 19:17:56 +0200

    Splashscreen works

--- src/Splash.tsx
+++ src/Splash.tsx
@@ -1,22 +1,30 @@
-import { Grow, Box, SvgIcon } from "@material-ui/core";
-import { ReactComponent as Logo } from "./assets/doushikani_nofonts.svg";
+import { Grow, Grid, Typography } from "@material-ui/core";
+import { ReactComponent as Logo } from "./assets/doushikani.svg";
 
 interface Props {
   onFinish: () => void;
 }
 
 const Splash = (props: Props) => {
-  //setTimeout(props.onFinish, 3000);
+  setTimeout(props.onFinish, 3000);
 
   return (
-    <Grow>
-      <Box height="100vh" display="flex" flexDirection="column">
-        <SvgIcon>
-          <Logo/>
-        </SvgIcon>
-      </Box>
-    </Grow>    
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grow in timeout={1000}>
+        <Grid item xs={12}>
+          <Logo />
+          <Typography variant="h2">doushikani</Typography>
+        </Grid>
+      </Grow>
+    </Grid>
   );
-}
+};
 
 export default Splash;
